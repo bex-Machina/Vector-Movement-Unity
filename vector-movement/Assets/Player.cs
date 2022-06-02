d0a291c5556041610fcebe0f484cca82af0b137f
bex-Machina <bex-Machina@users.noreply.github.com>
2022-06-02 11:43:36 +0100

    Player movement
    Added player movement using input manager and vector3

---
+++ vector-movement/Assets/Player.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Update is called once per frame
+    public float speed = 10f;
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * speed;
+        Vector3 amountToMove = velocity * Time.deltaTime;
+        transform.position += amountToMove;
+    }
+}
